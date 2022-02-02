8860190760193b8167f38c00717f463b68a4b368
Alessandro Mandaglio <ale.manda.30@gmail.com>
2022-02-02 16:37:34 +0100

    LAYOUT COMPLETO

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,90 @@
 <footer>
-
-</footer> 
+    <div class="footer-top-bg">
+        <div class="container">
+            <div class="option-container">
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" :alt="buycomic">
+                    <span>Digital comics</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}" :alt="buycomic">
+                    <span>Dc merchandise</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" :alt="buycomic">
+                    <span>Comic shop locator</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" :alt="buycomic">
+                    <span>Subscribe</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" :alt="buycomic">
+                    <span>dc power visa</span>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="footer-main-bg">
+        <div class="container">
+            <div class="footer-menu-sx">
+                <div class="list-container">
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">Tv</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                    <ul>
+                        <h3>SHOP</h3>
+                        <li><a href="">Shop DC</a></li>
+                        <li><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                    <ul>
+                        <h3>DC</h3>
+                        <li><a href="">Terms Of Use</a></li>
+                        <li><a href="">Privacy policy (New)</a></li>
+                        <li><a href="">Ad Chioces</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certicates</a></li>
+                        <li><a href="">Ratings</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact US</a></li>
+                    </ul>
+                    <ul>
+                        <h3>SITES</h3>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-logo">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="logo-footer">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom-sx">
+                <button class="btn"><span>Sign-up now!</span></button>
+            </div>
+            <div class="footer-bottom-dx">
+                <img src="{{asset('img/footer-facebook.png')}}">
+                <img src="{{asset('img/footer-periscope.png')}}">
+                <img src="{{asset('img/footer-pinterest.png')}}">
+                <img src="{{asset('img/footer-twitter.png')}}">
+                <img src="{{asset('img/footer-youtube.png')}}">
+            </div>
+        </div>
+    </div>
+</footer>
